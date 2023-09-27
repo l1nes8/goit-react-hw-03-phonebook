0cca4f782ed29c4b612fd1fa1a2c760203f08e6e
l1nes8 <126972112+l1nes8@users.noreply.github.com>
2023-09-27 13:51:17 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
+import ContactList from './ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = (name, number) => {
+    const { contacts } = this.state;
+    const newContact = {
+      name,
+      number,
+      id: nanoid(),
+    };
+    this.setState({
+      contacts: [...contacts, newContact],
+    });
+  };
+
+  deleteContact = contactId => {
+    const { contacts } = this.state;
+    const updatedContacts = contacts.filter(
+      contact => contact.id !== contactId
+    );
+    this.setState({
+      contacts: updatedContacts,
+    });
+  };
+
+  handleFilterChange = filter => {
+    this.setState({ filter });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  render() {
+    const { filter, contacts } = this.state;
+    const filteredContacts = this.getFilteredContacts();
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onAddContact={this.addContact} contacts={contacts} />
+        <h2>Contacts</h2>
+        <Filter filter={filter} onFilterChange={this.handleFilterChange} />
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
